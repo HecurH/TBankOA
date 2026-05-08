1d572aaec1b0abb92c62148edfc60ab3c545863a
HecurH <hecker@hecur.ru>
2026-05-08 16:38:35 +0300

    modified:   TBankOA/types/annotations.py  - фикс обработки datetime

--- TBankOA/types/annotations.py
+++ TBankOA/types/annotations.py
@@ -1,20 +1,17 @@
-
-
 from datetime import datetime, timezone
 from typing import Annotated
 
 from pydantic import PlainSerializer
 
 
-def datetime_to_iso(dt: datetime | None) -> str | None:
-    if dt is None:
-        return None
+def datetime_to_iso(dt: datetime) -> str:
     if dt.tzinfo is None or dt.utcoffset() is None:
         raise ValueError("Datetime must be timezone-aware")
-    return dt.replace(microsecond=0).astimezone().isoformat() 
+
+    return dt.astimezone(timezone.utc).replace(microsecond=0).isoformat()
 
 
 TBankDateTime = Annotated[
     datetime,
-    PlainSerializer(datetime_to_iso, return_type=str)
+    PlainSerializer(datetime_to_iso, when_used="json")
 ]
